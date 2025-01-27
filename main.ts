e94d8f8469d319f5cb6f24def6c66d629a77e248
Cocdrilo <alejandrolana1@gmail.com>
2025-01-27 18:56:01 +0100

    Commit sin cosas de Variables de entorno

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ import { resolvers } from "./resolvers.ts";
 import {Collection, MongoClient, ObjectId} from "mongodb"
 import { schema } from "./schema.ts";
 
-const mongoURL = Deno.env.get("mongoURL");
+/*const mongoURL = Deno.env.get("mongoURL");
 
 if (!mongoURL) {
   throw new Error("La variable de entorno 'mongoURL' no está configurada");
@@ -18,11 +18,14 @@ if (!ninjaApiKey) {
   throw new Error("La variable de entorno 'apiNinjaKey' no está configurada");
 }
 
+
 const client = new MongoClient (mongoURL)
 await client.connect()
 const dataBase = client.db('DBName')
 
 //export const xCollection = dataBase.collection<>('nameOfCollection')
+
+ */
 
 const server = new ApolloServer({
   typeDefs : schema,
