59b3b6d8f2a5b41a143d116757c1537ea62f9c3e
Cocdrilo <alejandrolana1@gmail.com>
2025-01-27 17:59:43 +0100

    Commit Inicial con el main hecho y nada más

---
+++ main.ts
@@ -0,0 +1,32 @@
+//Alejandro Lana
+import {ApolloServer} from "@apollo/server"
+import {startStandaloneServer} from "@apollo/server/standalone";
+import { schema } from "./schema.ts";
+import { resolvers } from "./resolvers.ts";
+import {Collection, MongoClient, ObjectId} from "mongodb"
+
+const mongoUrl = "mongodb+srv://cocdrilo:cocdrilo@nebrijatest.n7ral.mongodb.net/?retryWrites=true&w=majority&appName=NebrijaTest";
+
+const client = new MongoClient (mongoUrl)
+await client.connect()
+const dataBase = client.db('DBName')
+
+export const xCollection = dataBase.collection<>('nameOfCollection')
+
+if(!mongoUrl){
+  console.error('no url in env')
+  console.error('Error en la URL')
+}
+
+const server = new ApolloServer({
+  typeDefs : schema,
+  resolvers
+});
+
+const { url } = await startStandaloneServer(server, {
+  context:async () => ({}),
+  listen: { port: 8080 },
+});
+
+
+console.log(`Server running on: ${url}`);
